5c9db2f929db0ddc3638599a733e971c497865de
Waihon Yew <yewwaihon@gmail.com>
2021-03-09 21:42:24 +0800

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Waihon Yew on 3/9/21.
+//  Copyright © 2021 Waihon Yew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
